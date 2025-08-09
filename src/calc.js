a9a2fd67f69136c1239286b0a3bcc56bb18445a0
innofro555-arch <innofro555@gmail.com>
2025-08-10 01:51:14 +0900

    Delete src directory

--- src/calc.js
+++
@@ -1,8 +0,0 @@
-export function add(a, b) {
-  return a + b;
-}
-
-export function multiply(a, b) {
-  // BUG: 本来は a * b
-  return a + b;
-}
--- src/src/index.js
+++
@@ -1,4 +0,0 @@
-import { add, multiply } from "./calc.js";
-
-console.log("add(2, 3) =", add(2, 3));
-console.log("multiply(2, 3) =", multiply(2, 3)); // 本来は6だが今は5
